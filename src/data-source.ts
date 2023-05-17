fade77b71376130dd144f453ab2e7ce67019a995
Matheus Dávila <110111770+drmatheus@users.noreply.github.com>
2023-05-17 11:46:09 -0300

    Remove unused variable

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,6 @@ const dataSourceConfig = (): DataSourceOptions => {
   const migrations: string = path.join(__dirname, "./migrations/**.{ts,js}");
   const entities: string = path.join(__dirname, "./entities/**.{ts,js}");
   const databaseURL: string | undefined = process.env.DATABASE_URL;
-  const nodeEnv: string | undefined = process.env.NODE_ENV;
 
   if (!databaseURL) {
     throw new Error("Env var DATABASE_URL is undefined!");
